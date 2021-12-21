5c8b357b0f74a36629f9a58c7c04abeb1c9de93c
GEVORG KEROBYAN <kerobyangevorg@gmail.com>
2021-12-21 10:18:09 +0400

    Assignment 04

---
+++ classWork11_04.js
@@ -0,0 +1,39 @@
+// A teacher has created a gradeLabs function that verifies if student programming labs work. This function
+// loops over an array of JavaScript objects that should contain a student property and runLab property.
+// The runLab property is expected to be a function containing the student's code. The runLab function is
+// called and the result is compared to the expected result. If the result and expected results don't match,
+// then the lab is considered a failure.
+// gradeLabs
+// The gradeLabs function works for the majority of cases. However, what happens if a student named their
+// function incorrectly? Run gradeLabs and pass it studentLabs2 as defined below.
+
+
+function gradeLabs(labs) {
+    try {
+        for (let i = 0; i < labs.length; i++) {
+            let lab = labs[i];
+            let result = lab.runLab(3);
+            console.log(`${lab.student} code worked: ${result === 27}`);
+        } throw new Error(`: Error thrown`);
+    }   catch (Error) {
+        return Error;
+    }
+}
+
+let studentLabs = [
+    {
+        student: "Carly",
+        runLab: function (num) {
+            return Math.pow(num, num);
+        },
+    },
+    {
+        student: "Erica",
+        runLab: function (num) {
+            return num * num;
+        },
+    },
+]; 
+
+console.log(gradeLabs(studentLabs));
+
